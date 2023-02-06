2fd57a9bc6d8defb802c7c0158a149bd2fed0cef
zeed3v <felipebrionese@gmail.com>
2023-02-06 16:50:43 -0300

    estructura

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,27 +1,43 @@
 import React from 'react';
+import color from '../assets/color.png';
 
 function Banner() {
     return (
       <div className="container flex mx-auto flex-col md:flex-row">
             <div className="row">
-                <div className="col-6 items-center">
+                <div className="col-6">
                     <h2 className="font-semibold">
                         ¿Cómo funcionamos?
                     </h2>
-                    <h5>
-                        Da click a nuestro video y en sólo 60 segundos conoce y
-                        cómo estamos cambiando el mundo TI, acelerando y potenciando las células tecnológicas.
-                    </h5>
                 </div>
                 <div className="col-6">
                     <h1>
                         DEVSAFIO
                     </h1>
-                    <p>
-                        El puente para impulsar, potenciar y acelerar las carreras de programadores y de la
-                        industria tecnológica
-                    </p>
+                    <div className='row'>
+                        <p className='col-11'>
+                            El puente para impulsar, potenciar y acelerar las carreras de programadores y de la
+                            industria tecnológica
+                        </p>
+                        <img src={color} alt="color" className='col-1' />
+                    </div>
                 </div>
+                <div className='row'>
+                    <div className='col-6'>
+                        <h5>
+                            Da click a nuestro video y en sólo 60 segundos conoce y
+                            cómo estamos cambiando el mundo TI, acelerando y potenciando las células tecnológicas.
+                        </h5>
+                    </div>
+                    <div className='col-6'>
+                        <h3>
+                            ¿Cómo funcionamos?
+                        </h3>
+                        <p>
+                            Te lo explico en 4 pasos
+                        </p>
+                    </div>
+                </div> 
             </div>
       </div>
     )
