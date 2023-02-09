4db616e4ed2463ed2b8b57c01a95b0fbcf973314
zeed3v <felipebrionese@gmail.com>
2023-02-09 14:42:50 -0300

    style on HTML 2

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,8 +11,8 @@ function Banner() {
                     </h2>
                     <div className='py-4 px-4'>
                         <h5>
-                            Da click a nuestro video y en sólo 60 segundos conoce y
-                            cómo estamos cambiando el mundo TI, acelerando y potenciando las células tecnológicas.
+                            Da click a nuestro video y en sólo 60 segundos conoce <span className='text-primary fw-bold'>Devsafio </span>
+                            cómo estamos <span className='text-primary fw-bold'>cambiando el mundo TI</span>, acelerando y potenciando las células tecnológicas.
                         </h5>
                     </div>
                 </div>
