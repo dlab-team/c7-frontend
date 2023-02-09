dbed10f48938cf157e4cee2e891c5a0b1f779ba4
zeed3v <felipebrionese@gmail.com>
2023-02-09 14:07:56 -0300

    video added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,42 +1,25 @@
 import React from 'react';
-import color from '../assets/color.png';
+import Video from './Video';
 
 function Banner() {
     return (
-      <div className="container flex mx-auto flex-col md:flex-row">
-            <div className="row">
-                <div className="col-6">
+      <div className="container">
+            <div className="row align-items-center">
+                <div className="col-6 text-center mx-auto">
                     <h2 className="font-semibold">
                         ¿Cómo funcionamos?
                     </h2>
-                </div>
-                <div className="col-6">
-                    <h1>
-                        DEVSAFIO
-                    </h1>
-                    <div className='row'>
-                        <p className='col-11'>
-                            El puente para impulsar, potenciar y acelerar las carreras de programadores y de la
-                            industria tecnológica
-                        </p>
-                        <img src={color} alt="color" className='col-1' />
-                    </div>
-                </div>
-                <div className='row'>
-                    <div className='col-6'>
+                    <div className='py-4 px-4'>
                         <h5>
                             Da click a nuestro video y en sólo 60 segundos conoce y
                             cómo estamos cambiando el mundo TI, acelerando y potenciando las células tecnológicas.
                         </h5>
                     </div>
-                    <div className='col-6'>
-                        <h3>
-                            ¿Cómo funcionamos?
-                        </h3>
-                        <p>
-                            Te lo explico en 4 pasos
-                        </p>
-                    </div>
+                </div>
+                <div className='col-6'>
+                    <h3>
+                        <Video />
+                    </h3>
                 </div> 
             </div>
       </div>
