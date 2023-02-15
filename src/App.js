f45aeda35fa0d3d1314223e208bad8196a379b76
Lizander Camilo del Sol <lizander.kmilo@gmail.com>
2023-02-15 15:19:03 -0300

    componente DevSafiamos home_system C7-52

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.scss';
+import SectionHome_system from './components/Section_home_system';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SectionHome_system />
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/components/Section_home_system/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import imgHomeSystem from '../../assets/DevSafio_sistema.svg'
+import './styles/styles_home_system.scss'
+
+const SectionHome_system = () => {
+    return <>
+        <div className='container-home-section'>
+
+            <div className='subject--text--title'>DEVsafiamos el sistema</div>
+
+            <div className='info--text--normal'>
+                <span>
+                    ¿Escasez de talento Senior? No te preocupes, en  <span className='fw-bold text-primary' >Devsafio </span>encontrarás el talento que necesitas en nuestro programa Mentorías.
+                </span>
+            </div>
+            <div className='info--text--normal'>
+                <span>
+                    Completa el formulario y te contactaremos en breve para acompañarte en la búsqueda de Talento TI que necesitas.
+                </span>
+            </div>
+            <div className='image-home-system'> <img src={imgHomeSystem} alt='imagen chica pensando' /> </div>
+
+            <div className='subject--text--title'>
+                <span>
+                    ¡Agenda una asesoría con nosotros si quieres saber más sobre cómo acelerar y potenciar tu próximo Talento TI!
+                </span>
+            </div>
+
+            <a className='btn-dev btn-blue btn-agendar-homeSystem' href='/'>Agendar</a>
+        </div>
+    </>
+
+}
+
+export default SectionHome_system
