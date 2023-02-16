5f89a128604053162412eb2ccd1aed597ddd4214
Lizander Camilo del Sol <lizander.kmilo@gmail.com>
2023-02-16 09:59:54 -0300

    estilos de botones globales del formulario y header, sin la estructura moderna de carpetas del proyecto

--- src/App.js
+++ src/App.js
@@ -17,20 +17,6 @@ function App() {
         >
           Learn React
         </a>
-
-        <div
-          style={{ width: '400px', height: '500px', background: '#4545' }}
-        >
-          <a className="btn-dev btn-blue"> PRUEBA 1</a>
-          <a className="btn-dev btn-blue btn-body-section"> PRUEBA 2</a>
-          <a className="btn-dev btn-blue btn-body-section"> PRUEBA 3</a>
-          <a className="btn-dev btn-white btn-body-section"> PRUEBA 3</a>
-          <a className="btn-dev btn-blue btn-body-section"> PRUEBA 3</a>
-          {/* <span className='big--text--title'> hola mundo probando 123 </span> <br/> */}
-          {/* <span className='medium--text--title'> hola mundo probando 456 </span> <br/> */}
-          <span className='subject--text--title'> hola mundo probando 789 </span> <br/>
-          <span className='info--text--normal'> hola mundo probando 554 </span> <br/>
-        </div>
       </header>
     </div>
   );
