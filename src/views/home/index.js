c049d7b38c8177997020a4ea9a1b8abbdcf52121
Federico Puppo <federico.puppo@hotmail.com>
2023-02-18 15:58:01 -0300

    added section component with structure

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import Section from './components/section/Section.js';
 
 // encargada de renderizar todos los componentes de la vista home
 const Ui_home = () => {
     return (
-        <div>Ui_home</div>
+        <div>Ui_home
+            <Section/>
+        </div>
         // <Componente_Banner/>
         // <Componente_Seccion1/>
         // <Componente_Seccion_Galleria/>
