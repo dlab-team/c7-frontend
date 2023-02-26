bfd032308d5cfd21e66847804d19cb56115cadfd
arrdamian <arrdamian@icloud.com>
2023-02-26 16:32:41 -0300

    add Footer component

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import SectionSinJrsNoHaySrs from '../../components/Section_sinJrsNoHaySrs/SectionSinJrsNoHaySrs.js';
+import Footer from '../../components/Footer/Footer.js';
 
 // encargada de renderizar todos los componentes de la vista home
 const Ui_home = () => {
     return (
         <div>Ui_home
             <SectionSinJrsNoHaySrs/>
+            <Footer />
         </div>
         // <Componente_Banner/>
         // <Componente_Seccion1/>
         // <Componente_Seccion_Galleria/>
         // <Componente_SeccionX/>
         // <Componente_SeccionY/>
-        // <Componente_Footer/>
     )
 }
 
