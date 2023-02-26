5091dff72ec641497fad9f28b411b767e94b15b0
arrdamian <arrdamian@icloud.com>
2023-02-26 15:57:29 -0300

    add navbar component

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -6,6 +6,7 @@ import Navbar from '../../components/Navbar/Navbar.js';
 const Ui_home = () => {
     return (
         <div>
+            //navbar
             <Navbar />
             <SectionSinJrsNoHaySrs/>
         </div>
