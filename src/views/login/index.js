eb161b99405038cda00b092721454f0ddd48a98b
Lizander Camilo del Sol <lizander.kmilo@gmail.com>
2023-02-27 11:21:28 -0300

    vista login arreglada, componentes BEM implementados correctamente, <UiLogin/> v2

--- src/views/login/index.js
+++ src/views/login/index.js
@@ -9,21 +9,20 @@ const UiLogin = () => {
         <div className='login-container'>
 
             <div className='login-modal'>
-                <div className='subject__text__title'> Unete a Devsafío </div>
+                <div className='text__subject'> Unete a Devsafío </div>
 
                 <div className='login-modal__text'>
                     <span>Crea tu cuenta profesional en DevSafío para que seas parte de distintas ofertas laborales que tenemos junto a importantes empresas en latinoamérica.</span>
                 </div>
 
-                <div className='login-modal__buttons'>
-                    <a className='btn__dev btn-white'> <Envelope className='login--icons' />  Ingresa con tu mail</a>
-                    <a className='btn__dev btn-white'><Google className='login--icons' /> Ingresa con tu Google</a>
-                    <a className='btn__dev btn-linkedin'><Linkedin className='login--icons' />Ingresa con tu LinkedIn</a>
-                    <a className='btn__dev btn-github'><Twitter className='login--icons' />Ingresa con tu Twitter</a>
-                    <a className='btn__dev btn-twitter'><Github className='login--icons' />Ingresa con tu GitHub</a>
+                <div className='login-modal__box-buttons'>
+                    <a className='btn-app login-modal__button btn-app--white btn--border'> <Envelope className='login--icons' />  Ingresa con tu mail</a>
+                    <a className='btn-app login-modal__button btn-app--white btn--border'><Google className='login--icons' /> Ingresa con tu Google</a>
+                    <a className='btn-app login-modal__button btn--linkedin'><Linkedin className='login--icons' />Ingresa con tu LinkedIn</a>
+                    <a className='btn-app login-modal__button btn--github'><Twitter className='login--icons' />Ingresa con tu Twitter</a>
+                    <a className='btn-app login-modal__button btn--twitter'><Github className='login--icons' />Ingresa con tu GitHub</a>
                 </div>
             </div>
-
         </div>
     )
 }
