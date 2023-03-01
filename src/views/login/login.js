b4a02646d2998db64f06a306df2490dc4f288ec1
Lizander Camilo del Sol <lizander.kmilo@gmail.com>
2023-03-01 12:24:44 -0300

    estructurando nombre de la vista a login.js y login.scss

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './style.scss'
+import './login.scss'
 import { Envelope, Google, Linkedin, Twitter, Github } from 'react-bootstrap-icons'
 
 const UiLogin = () => {
