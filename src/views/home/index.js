9b022d3dee112968291b1aec319ce84f5532c7e4
Federico Puppo <federico.puppo@hotmail.com>
2023-03-01 18:10:08 -0300

    view fixed - margins adjusted

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,30 +1,36 @@
-import React from 'react'
-import SectionSinJrsNoHaySrs from '../../components/Section_sinJrsNoHaySrs/SectionSinJrsNoHaySrs.js';
-import Navbar from '../../components/Navbar/Navbar.js';
-import Footer from '../../components/Footer/Footer.js';
-import SectionHomeSystem from '../../components/Section_home_system/index.js';
-import Banner from '../../components/Banner/Banner.js';
-import BigBanner from '../../components/BigBanner/BigBanner.js';
-
-
+import React from "react";
+import SectionSinJrsNoHaySrs from "../../components/SectionSinJrsNoHaySrs/index";
+import Navbar from "../../components/Navbar/Navbar.js";
+import Footer from "../../components/Footer/Footer.js";
+import SectionHomeSystem from "../../components/Section_home_system/index.js";
+import Banner from "../../components/Banner/Banner.js";
+import BigBanner from "../../components/BigBanner/BigBanner.js";
+import ContactForm from "../../components/ContactForm/index.js";
 
 // encargada de renderizar todos los componentes de la vista home
 const Ui_home = () => {
-    return (
-        <div>
-            <Navbar />
-            <BigBanner />
-            <Banner />
-            <SectionSinJrsNoHaySrs/>
-            <SectionHomeSystem/>
-            <Footer />
+  return (
+    <div>
+      <Navbar />
+      <BigBanner />
+      <Banner />
+      <SectionSinJrsNoHaySrs />
+      <div className="row">
+        <div className="col-md-6">
+          <SectionHomeSystem />
         </div>
-        // <Componente_Banner/>
-        // <Componente_Seccion1/>
-        // <Componente_Seccion_Galleria/>
-        // <Componente_SeccionX/>
-        // <Componente_SeccionY/>
-    )
-}
+        <div className="col-md-6">
+          <ContactForm />
+        </div>
+      </div>
+      <Footer />
+    </div>
+    // <Componente_Banner/>
+    // <Componente_Seccion1/>
+    // <Componente_Seccion_Galleria/>
+    // <Componente_SeccionX/>
+    // <Componente_SeccionY/>
+  );
+};
 
-export default Ui_home
+export default Ui_home;
