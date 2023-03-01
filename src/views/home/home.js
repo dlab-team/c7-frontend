59b99a7ca9346642b6c56682a5477ca5d49353ba
Lizander Camilo del Sol <lizander.kmilo@gmail.com>
2023-03-01 12:54:47 -0300

    arreglando nombre del componente index por home.js

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -10,7 +10,7 @@ const UiHome = () => {
         <div>
             <Navbar />
             <Banner />
-            <SectionSinJrsNoHaySrs />
+            <SectionSinJrsNoHaySrs/>
             <Footer />
         </div>
     )
