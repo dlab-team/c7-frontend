7f97d8a6b176b28938b3c50d9b5b292297980864
Lizander Camilo del Sol <lizander.kmilo@gmail.com>
2023-03-01 10:46:15 -0300

    BigBanner implementado, arreglado <newContrat> por <NewContract/> y una falta de ortografia

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -3,12 +3,14 @@ import SectionSinJrsNoHaySrs from '../../components/Section_sinJrsNoHaySrs/Secti
 import Navbar from '../../components/Navbar/Navbar.js';
 import Footer from '../../components/Footer/Footer.js';
 import Banner from '../../components/Banner/Banner.js';
+import BigBanner from '../../components/BigBanner/BigBanner.js';
 
 // encargada de renderizar todos los componentes de la vista home
 const Ui_home = () => {
     return (
         <div>
             <Navbar />
+            <BigBanner />
             <Banner />
             <SectionSinJrsNoHaySrs/>
             <Footer />
