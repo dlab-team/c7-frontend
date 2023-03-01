defd33d5bbfaef50a228d072191c25d5ea28ef5a
Federico Puppo <federico.puppo@hotmail.com>
2023-03-01 18:13:53 -0300

    view fixed - margins adjusted

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
 
 
 
