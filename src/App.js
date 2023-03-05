85c9e1e4104f3993a7f061ff1e30182ef5cf1c25
arrdamian <arrdamian@icloud.com>
2023-03-05 11:17:55 -0300

    Add react router dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './styles/App.scss';
 import UiHome from './views/home/home';
+import { BrowserRouter } from 'react-router-dom';
 //Redux
 import { Provider } from 'react-redux';
 import store from './utils/Redux/store';
@@ -8,9 +9,11 @@ import store from './utils/Redux/store';
 function App() {
   return (
     <>
-      <Provider store={store}>
-        <UiHome />
-      </Provider>
+      <BrowserRouter>
+        <Provider store={store}>
+          <UiHome />
+        </Provider>
+      </BrowserRouter>
     </>
 )}
 
