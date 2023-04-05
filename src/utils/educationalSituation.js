4a0d3b6f181db7461861501b8f7b7015cc44643b
Federico Puppo <federico.puppo@hotmail.com>
2023-04-05 18:17:59 -0300

    Added selection to forms

---
+++ src/utils/educationalSituation.js
@@ -0,0 +1,26 @@
+export const educationalSituation = [
+  {
+    name: "Egresado/a",
+    value: "Egresado/a",
+  },
+  {
+    name: "Titulado/a",
+    value: "Titulado/a",
+  },
+  {
+    name: "En Curso",
+    value: "En Curso",
+  },
+  {
+    name: "Inconclusa",
+    value: "Inconclusa",
+  },
+  {
+    name: "Estudios técnicos o universitarios en otras áreas",
+    value: "Estudios técnicos o universitarios en otras áreas",
+  },
+  {
+    name: "Otra",
+    value: "Otra",
+  },
+];
