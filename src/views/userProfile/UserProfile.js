b0f1bf9d3b0bf1d270500663f34a06667be59b31
Damián Arrieta <104109555+damian-arrieta@users.noreply.github.com>
2023-04-05 10:22:32 -0300

    C7 125 (#79)
    * HOTFIX: Email validation
    
    * HOTFIX: Userprofile view
    
    * Revert "HOTFIX: Userprofile view"
    
    This reverts commit 956b8c0734c451957f7ee21cb7d93b16ae74ca67.
    
    wrong main
    
    * HOTFIX: Fix UserProfile view
    
    * Fixed problems

--- src/views/userProfile/UserProfile.js
+++ src/views/userProfile/UserProfile.js
@@ -4,6 +4,8 @@ import MenuProfile from '../../components/menuProfiles/menuProfile'
 import HomeUserProfile from '../../components/homeProfile/homeProfile'
 import PersonalProfile from '../../components/personalProfile/personalProfile'
 import TestProfile from '../../components/testProfile/testProfile'
+import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
 
 const UserProfile = () => {
     const [choice_selected, setChoice] = useState('home')
@@ -14,17 +16,23 @@ const UserProfile = () => {
     }
 
     return (
-        <section className='user-profile'>
-            <div className='user-profile__menu'>
-                <MenuProfile choice_selected={setChoice} />
-            </div>
-            <div className='user-profile__content'>
-                {menuRender[choice_selected]}
-                {/*choice_selected === 'home' && <HomeUserProfile />}
-                {choice_selected === 'perfil' && <PersonalProfile />}
-                {choice_selected === 'test' && <TestProfile />} */}
-            </div>
-        </section>
+        <>
+            <Navbar />
+
+            <section className='user-profile'>
+                <div className='user-profile__menu'>
+                    <MenuProfile choice_selected={setChoice} />
+                </div>
+                <div className='user-profile__content'>
+                    {menuRender[choice_selected]}
+                    {/*choice_selected === 'home' && <HomeUserProfile />}
+                    {choice_selected === 'perfil' && <PersonalProfile />}
+                    {choice_selected === 'test' && <TestProfile />} */}
+                </div>
+            </section>
+
+            <Footer />
+        </>
     )
 }
 
