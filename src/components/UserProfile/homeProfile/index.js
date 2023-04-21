be5001333fef3aec2735414a6943fe503cef82ac
Lizander Camilo del Sol <lizander.kmilo@gmail.com>
2023-04-21 18:54:52 -0400

    texto introducido para que el usuario tenga referencia de que ve en pantalla

--- src/components/UserProfile/homeProfile/index.js
+++ src/components/UserProfile/homeProfile/index.js
@@ -6,7 +6,12 @@ const HomeUserProfile = () => {
     return (
         <div className='home-profile'>
             <div className='home-profile__title  text__title--medium'>¡Bienvenido!</div>
-            <div className='home-profile__subject text--normal'>Mapa para recibir ofertas automáticas</div>
+            <div className='home-profile__subject text--normal'>Estamos emocionados de tenerlo(a)
+             aquí y ayudar a conectarlo con oportunidades laborales emocionantes en su campo. A continuación, le mostraremos sencillos 
+            pasos que debe seguir para recibir ofertas laborales. 
+            ¡Gracias por unirse a la comunidad de <span className='text-primary fw-normal'> 
+            DevSafio </span>y esperamos ayudarlo(a) 
+            en su carrera en tecnología!</div>
             <div className='home-profile__cards'>
 
                 <div className='profile-card'>
