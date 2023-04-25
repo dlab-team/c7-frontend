cb2a782d895679edc327f7009a0a770fb973f0f3
Lizander Camilo del Sol <lizander.kmilo@gmail.com>
2023-04-25 13:30:34 -0400

    editar disponibilidad laboral

--- src/utils/Redux/store.js
+++ src/utils/Redux/store.js
@@ -9,6 +9,7 @@ import toolsReducer from "./Slices/Tools/index";
 import idealJob from "./Slices/IdealJob";
 import authDev from "./Slices/authDev";
 import userPersonalInfo from "./Slices/UserPersonalInfo";
+import aviableWork from "./Slices/aviableWork";
 
 const store = configureStore({
   reducer: {
@@ -20,6 +21,7 @@ const store = configureStore({
     idealJob: idealJob,
     authDev:authDev,
     userPersonalInfo:userPersonalInfo,
+    aviableWork:aviableWork
   },
 });
 
